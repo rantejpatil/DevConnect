ec4d15ea6b754cc227a85b0ff29574c777b2dca1
Rantej <rantejpatil1721@gmail.com>
2025-04-21 16:28:47 +0530

    changes for cors error

--- src/app.js
+++ src/app.js
@@ -4,12 +4,61 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+// app.use(express.json());
+// app.use(cookieParser());
+
+// app.use(cors());
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
+
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+    allowedHeaders: ['Content-Type'],
   })
 );
+
+// app.options("*", cors({
+//   origin: "http://localhost:5173",
+//   credentials: true,
+//   methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+//   allowedHeaders: ['Content-Type','Authorization'],
+// })
+// );
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], // must include OPTIONS
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
+
+// // ✅ Must add this line to handle OPTIONS preflight requests
+// app.options("*", cors());
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+//   credentials: true,
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+//   // Handle preflight requests
+//   if (req.method === 'OPTIONS') {
+//     return res.sendStatus(200);
+//   }
+//   next();
+// });
+
+
 
 app.use(express.json());
 app.use(cookieParser());
@@ -34,3 +83,4 @@ connectDB()
   .catch((err) => {
     console.error("Database cannot be connected!!");
   });
+
